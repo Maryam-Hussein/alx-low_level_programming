86692b14a0f511b3bc7d3ace08d4df5fc8fb6de4
MaryamHussein <eng.maryamhussein@gmail.com>
2023-08-15 13:29:30 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,11 @@
 int main(void)
 {
 int i;
-	char p[] = "Holberton";
-
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
-	return (0);
+        char p[] ="Main"
+ for (i = 0; i < 9; i++)
+                {
+                _putchar(p[i]);
+                }
+                _putchar('\n');
+        return (0);
 }
