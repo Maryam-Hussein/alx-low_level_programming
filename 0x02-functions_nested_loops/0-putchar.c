3da3099c5cebe79ec43c6de2f6bdf1abc120dd8b
MaryamHussein <eng.maryamhussein@gmail.com>
2023-08-15 14:13:52 -0700

    try

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
 #include "main.h"
+/**
+* main - Print new line
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
         int i;
         char p[] = "\n";
 
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
-	return (0);
+        for (i = 0; i < 9; i++)
+                {
+                _putchar(p[i]);
+                }
+                _putchar('\n');
+        return (0);
 }
