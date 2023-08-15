2b1dbeddf342251ba706c7e70f05f872addf450c
MaryamHussein <eng.maryamhussein@gmail.com>
2023-08-15 13:23:07 -0700

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+	char p[] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
+	return (0);
+}
